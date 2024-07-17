794e2d3f0d35113bc5974c210374c07253efb0e8
S.W. Schilke <swschilke@users.noreply.github.com>
2024-07-17 14:59:49 +0200

    Add files via upload
    Usage: run in Powershell podrssload.ps1 <URL to Podcast RSS Feed>

---
+++ podrssload.ps1
@@ -0,0 +1,20 @@
+﻿ If ($args[0] -ne $null) {
+ [xml]$feed = Invoke-WebRequest -Uri $args[0].ToString()
+ Write-Host " "
+ Write-Host "Podcast Rss Feed is " $args[0].ToString()
+ Write-Host " "
+ Write-Host "Downloading Podcast " $feed.rss.channel.title.'#cdata-section'
+ Write-Host " "
+ Write-Host "Number of Episodes =" $Feed.rss.channel.Item.count
+ Write-Host " "
+ Invoke-WebRequest -Uri $args[0].ToString() -OutFile $args[0].ToString().Substring($args[0].ToString().LastIndexOf('/')+1)
+ Write-Host "Downloading Podcast RSS"
+ Write-Host " "
+ for ($i=0;$i -lt $Feed.rss.channel.Item.count;$i++) {
+    $n = $i+1
+    Write-Host $n". Downloading MP3 " $feed.rss.channel.Item[$i].enclosure.url.Substring($feed.rss.channel.Item[$i].enclosure.url.LastIndexOf('/')+1)
+    Invoke-WebRequest -Uri $feed.rss.channel.Item[$i].enclosure.url -OutFile $feed.rss.channel.Item[$i].enclosure.url.Substring($feed.rss.channel.Item[$i].enclosure.url.LastIndexOf('/')+1)
+    }} 
+    else 
+    {Write-Host "USAGE: podrssload <URL to RSS Feed>"}
+    
