32b564baf63dd0e28a939d82fa453afad8be424f
S.W. Schilke <swschilke@users.noreply.github.com>
2024-07-19 10:41:46 +0200

    update URL Check

--- podrssload.ps1
+++ podrssload.ps1
@@ -1,9 +1,12 @@
 ﻿ If ($args[0] -ne $null) {
+ $tempstring= $args[0].ToString()
+ $regEx="^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$"
+iF ( $tempstring -match $regex) {
  [xml]$feed = Invoke-WebRequest -Uri $args[0].ToString()
  Write-Host " "
  Write-Host "Podcast Rss Feed is " $args[0].ToString()
  Write-Host " "
- Write-Host "Downloading Podcast " $feed.rss.channel.title.ToString() 
+ Write-Host "Downloading Podcast " $feed.rss.channel.title.ToString()
  Write-Host " "
  Write-Host "Number of Episodes =" $Feed.rss.channel.Item.count
  Write-Host " "
@@ -15,6 +18,13 @@
     Write-Host $n". Downloading MP3 " $feed.rss.channel.Item[$i].enclosure.url.Substring($feed.rss.channel.Item[$i].enclosure.url.LastIndexOf('/')+1)
     Invoke-WebRequest -Uri $feed.rss.channel.Item[$i].enclosure.url -OutFile $feed.rss.channel.Item[$i].enclosure.url.Substring($feed.rss.channel.Item[$i].enclosure.url.LastIndexOf('/')+1)
     }} 
+    else
+        {Write-Host " "
+         Write-Host "Invalid URL"
+         Write-Host " "}}
     else 
-    {Write-Host "USAGE: podrssload <URL to RSS Feed>"}
+    {
+    Write-Host " "
+    Write-Host "USAGE: podrssload <URL to RSS Feed>"
+    Write-Host " "}
     
